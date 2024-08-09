01c92f9f7f16f9d36584feac0ed59f75f24ecfb5
germansayhi <120578921+germansayhi@users.noreply.github.com>
2024-08-09 21:45:07 +0700

    Add spring swagger

--- src/main/java/com/vti/kshop/kshopdemo/KshopDemoApplication.java
+++ src/main/java/com/vti/kshop/kshopdemo/KshopDemoApplication.java
@@ -2,12 +2,26 @@ package com.vti.kshop.kshopdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class KshopDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KshopDemoApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://127.0.0.1:5500")
+						.allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE");
+			}
+		};
 	}
 
 }
