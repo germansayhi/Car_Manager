9e9a80845e86e237810df674c11a1ed1ad0e5da8
germansayhi <120578921+germansayhi@users.noreply.github.com>
2024-08-11 20:56:00 +0700

    Build method where continue

--- src/main/java/com/vti/kshop/kshopdemo/form/AccessoryFilterForm.java
+++ src/main/java/com/vti/kshop/kshopdemo/form/AccessoryFilterForm.java
@@ -3,8 +3,12 @@ package com.vti.kshop.kshopdemo.form;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 public class AccessoryFilterForm {
+    private String searchName;
+    private Long searchPrice;
     private String search;
 }
--- src/main/java/com/vti/kshop/kshopdemo/specification/AccessorySpecification.java
+++ src/main/java/com/vti/kshop/kshopdemo/specification/AccessorySpecification.java
@@ -9,6 +9,8 @@ import jakarta.persistence.criteria.Root;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.util.StringUtils;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 public class AccessorySpecification {
@@ -21,11 +23,26 @@ public class AccessorySpecification {
                     CriteriaBuilder builder
             ){
                 var predicates = new ArrayList<Predicate>();
+                var searchName = form.getSearchName();
+                if(StringUtils.hasText(searchName)){
+                    var pattern  = "%" + searchName + "%";
+                    var predicate = builder.like(
+                            root.get("name"), pattern);
+                    predicates.add(predicate);
+                }
+
+                var searchPrice = form.getSearchPrice();
+                if(searchPrice != null){
+                    var pattern  = Integer.toString(Math.toIntExact(searchPrice));
+                    var predicate = builder.equal(root.get("price"),pattern);
+                    predicates.add(predicate);
+                }
+
                 var search = form.getSearch();
                 if(StringUtils.hasText(search)){
                     var pattern  = "%" + search + "%";
                     var predicate = builder.like(
-                            root.get("name"), pattern);
+                            root.get("car").get("licensePlate"), pattern);
                     predicates.add(predicate);
                 }
                 return builder.and(predicates.toArray(new Predicate[0]));
